4e005944e842fce81f94e225cb2207c33779f135
Mica Oz <kahulaoz@gmail.coml>
2022-06-20 18:23:48 -0400

    push and pop

--- stack.js
+++ stack.js
@@ -22,6 +22,39 @@ class Stack {
     this.last = null;
     this.size = 0;
   }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    //delete head
+    //item2==head
+    //sever connection
+    //return oldHead
+    let oldHead = this.head;
+    this.head = oldHead.next;
+    oldHead.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.tail === null;
+    }
+    return oldHead;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    let oldHead = this.head;
+    //no head - set head and tail to this val
+    if (this.head === null && this.tail === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head = newNode;
+      this.head.next = oldHead;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 class Node {
@@ -30,3 +63,5 @@ class Node {
     this.next = null;
   }
 }
+
+let listStack = new Stack();
